a01990da9a7084556c6688153324ea6bbe0c2566
AndreiLesi <lesi.andrei@gmail.com>
2020-08-16 23:34:53 +0200

    Finished Entry Page, Index Page and started working on Search

--- Projekt_1/encyclopedia/views.py
+++ Projekt_1/encyclopedia/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import logging
 import markdown as md
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from . import util
 
+logger = logging.getLogger(__name__)
 
 def index(request):
+    if request.method == "POST":
+        entry = request.POST.get("entry")
+        if not request.POST.get("entry"):
+            return redirect("index")
+        return redirect("entry", entry=entry)
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def entry(request, name):
+
+def entry(request, entry):
     # Check if entry exists
-    if name in util.list_entries():
-        entryHTML = md.markdown(util.get_entry(name))
+    if entry in util.list_entries():
+        entryHTML = md.markdown(util.get_entry(entry))
     else:
         entryHTML = False
 
     return render(request, "encyclopedia/entry.html", {
         "entry": entryHTML,
-        "title": name
+        "title": entry
     })
-
